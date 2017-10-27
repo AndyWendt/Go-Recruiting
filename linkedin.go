bb4454dc8dbb0dfa03c8e662897267123e5dc894
Andy Wendt <andy@awendt.com>
2017-10-27 12:58:49 -0600

    Grabbing all of the records with dev type positions

--- linkedin.go
+++ linkedin.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"encoding/csv"
+	"strings"
 )
 
 func main() {
@@ -19,9 +20,14 @@ func main() {
 
 	reader := csv.NewReader(file)
 
-	firstName := 0
-	lastName := 1
-	email := 2
+	firstNameIndex := 0
+	lastNameIndex := 1
+	emailIndex := 2
+	//companyIndex := 3
+	positionIndex := 4
+	//connectedOnIndex := 5
+	//tagsIndex := 6
+	out := make([][]string, 0)
 
 
 	lineCount := 0
@@ -34,14 +40,38 @@ func main() {
 			printError(err)
 		}
 
-		fmt.Println(record[firstName], record[lastName], record[email]);
+
+		fmt.Println(record[firstNameIndex], record[lastNameIndex], record[emailIndex])
+
+
+		if hasPosition(record[positionIndex]) {
+			tt := make([]string, 0)
+			out = append(out, append(tt, record[firstNameIndex]))
+		}
+
 		fmt.Println("Record", lineCount, "is", record, "and has", len(record), "fields")
 
 		fmt.Println()
 		lineCount += 1
 	}
+
+	fmt.Println(len(out))
 }
 
 func printError(err error) (n int, error error) {
 	return fmt.Println("Error: ", err)
+}
+
+func hasPosition(testPosition string) (hasPosition bool) {
+	positions := [5]string{"dev", "developer", "engineer", "programmer", "code"}
+
+	for _, position := range positions {
+		containsPosition := strings.Contains(strings.ToLower(testPosition), position)
+
+		if containsPosition == true {
+			return true
+		}
+	}
+
+	return false
 }
