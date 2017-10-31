ded3059b754f6a5b519821d058d825a65846bf57
Andy Wendt <andy@awendt.com>
2017-10-31 11:08:14 -0600

    Workable matches being sent to csv file

--- linkedin.go
+++ linkedin.go
@@ -3,13 +3,50 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"encoding/csv"
 	"strings"
 	"log"
 	"github.com/joho/godotenv"
 	"net/http"
+	"time"
+	"encoding/json"
+	"net/url"
 )
+
+type WorkableCandidatesResponse struct {
+	Candidates []struct {
+		ID        string      `json:"id"`
+		Name      string      `json:"name"`
+		Firstname string      `json:"firstname"`
+		Lastname  string      `json:"lastname"`
+		Headline  interface{} `json:"headline"`
+		Account   struct {
+			Subdomain string `json:"subdomain"`
+			Name      string `json:"name"`
+		} `json:"account"`
+		Job struct {
+			Shortcode string `json:"shortcode"`
+			Title     string `json:"title"`
+		} `json:"job"`
+		Stage                  string      `json:"stage"`
+		Disqualified           bool        `json:"disqualified"`
+		DisqualificationReason interface{} `json:"disqualification_reason"`
+		HiredAt                interface{} `json:"hired_at"`
+		Sourced                bool        `json:"sourced"`
+		ProfileURL             string      `json:"profile_url"`
+		Address                interface{} `json:"address"`
+		Phone                  interface{} `json:"phone"`
+		Email                  string      `json:"email"`
+		Domain                 interface{} `json:"domain"`
+		CreatedAt              time.Time   `json:"created_at"`
+		UpdatedAt              time.Time   `json:"updated_at"`
+	} `json:"candidates"`
+	Paging struct {
+		Next string `json:"next"`
+	} `json:"paging"`
+}
 
 func main() {
 	err := godotenv.Load()
@@ -40,6 +77,7 @@ func main() {
 	//connectedOnIndex := 5
 	//tagsIndex := 6
 	devs := make([]map[string]string, 0)
+	candidates := make([][]string, 0)
 
 
 	//lineCount := 0
@@ -48,9 +86,9 @@ func main() {
 
 		if err == io.EOF {
 			break
-		} else if err != nil {
-			printError(err)
 		}
+
+		checkError("error reading file", err)
 
 		if false == hasPosition(record[positionIndex]) {
 			continue
@@ -68,21 +106,9 @@ func main() {
 		devs = append(devs, person)
 	}
 
+	candidates = getCandidates(os.Getenv("WORKABLE_URL") + "/candidates", devs, candidates)
 
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://vynyl.workable.com/spi/v3/candidates", nil)
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Authorization", "Bearer " + os.Getenv("WORKABLE_API_KEY"))
-	req.Header.Add("Content-Type", "application/json")
-	resp, err := client.Do(req)
-
-
-	if err != nil {
-		printError(err)
-	}
-
-	io.Copy(os.Stdout, resp.Body)
+	writeToFile("workable-candidates.csv", candidates)
 
 	data := make([][]string, 0)
 
@@ -92,18 +118,8 @@ func main() {
 		data = append(data, []string{dev["first_name"], dev["last_name"], dev["email"], dev["company"], dev["position"]})
 	}
 
-	file, err = os.Create("data/result.csv")
-	checkError("Cannot create file", err)
-	defer file.Close()
 
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	for _, value := range data {
-		err := writer.Write(value)
-		checkError("Cannot write to file", err)
-	}
-
+	writeToFile("devs.csv", data)
 
 
 	fmt.Println(len(devs))
@@ -113,7 +129,7 @@ func printError(err error) (n int, error error) {
 	return fmt.Println("Error: ", err)
 }
 
-func hasPosition(testPosition string) (hasPosition bool) {
+func hasPosition(testPosition string) bool {
 	positions := [5]string{"dev", "developer", "engineer", "programmer", "code"}
 
 	for _, position := range positions {
@@ -130,5 +146,66 @@ func hasPosition(testPosition string) (hasPosition bool) {
 func checkError(message string, err error) {
 	if err != nil {
 		log.Fatal(message, err)
+		os.Exit(1)
+	}
+}
+
+func getCandidates(url string, devs []map[string]string, workableCandidates [][]string) [][]string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", "Bearer " + os.Getenv("WORKABLE_API_KEY"))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+
+	checkError("error making request", err)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	checkError("couldn't read response", err)
+
+	var candidates WorkableCandidatesResponse
+	err = json.Unmarshal(body, &candidates)
+	checkError("couldn't unmarshall response", err)
+
+
+	for _, candidate := range candidates.Candidates {
+		for _, dev := range devs {
+			if strings.ToLower(dev["first_name"]) == strings.ToLower(candidate.Firstname) && strings.ToLower(dev["last_name"]) == strings.ToLower(candidate.Lastname) {
+				workableCandidates = append(workableCandidates, []string{dev["first_name"], dev["last_name"], dev["email"], dev["company"], dev["position"]})
+			}
+		}
+	}
+
+	if isValidUrl(candidates.Paging.Next) {
+		fmt.Println("Calling getCandidates again: " + candidates.Paging.Next)
+		return getCandidates(candidates.Paging.Next, devs, workableCandidates)
+	}
+
+	return workableCandidates
+}
+
+func writeToFile(fileName string, data [][]string) bool {
+	file, err := os.Create("data/" +  fileName)
+	checkError("Cannot create file", err)
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, value := range data {
+		err := writer.Write(value)
+		checkError("Cannot write to file", err)
+	}
+
+	return true
+}
+
+func isValidUrl(toTest string) bool {
+	_, err := url.ParseRequestURI(toTest)
+	if err != nil {
+		return false
+	} else {
+		return true
 	}
 }
